329878c8dbf36b0986de8859b40f08801786577c
Taketoshi Takeuchi <goodgolf722003_2@yahoo.co.jp>
2019-12-18 02:03:04 +0900

    ファイル追加

--- notice.rb
+++ notice.rb
@@ -5,23 +5,32 @@ require 'open-uri'
 
 
 Slack.configure do |config|
-  config.token = 'xoxb-594546582053-853798190228-TNHYQwyhJI3U0Su28q4MlD8e'
+  config.token = ''
 end
 
-# #F&gindex取得
-# url = "https://money.cnn.com/data/fear-and-greed/"
-# charset = nil
-#
-# html = open(url) do |f|
-#     charset = f.charset
-#     f.read
-# end
-#
-# doc = Nokogiri::HTML.parse(html, nil, charset)
+#F&gindex取得
+url = "https://money.cnn.com/data/fear-and-greed/"
+charset = nil
 
+html = open(url) do |f|
+    charset = f.charset
+    f.read
+end
 
-# results = doc.xpath("//*[@id='needleChart']/ul").inner_text
+doc = Nokogiri::HTML.parse(html, nil, charset)
+
+p doc
+
+results1 = doc.xpath("//*[@id='needleChart']/ul/li[1]").inner_text
+results2 = doc.xpath("//*[@id='needleChart']/ul/li[2]").inner_text
+results3 = doc.xpath("//*[@id='needleChart']/ul/li[3]").inner_text
+results4 = doc.xpath("//*[@id='needleChart']/ul/li[4]").inner_text
+results5 = doc.xpath("//*[@id='needleChart']/ul/li[5]").inner_text
 
 
 client = Slack::Web::Client.new
-client.chat_postMessage(channel: '#taketaketest', text: 'aaaa')
+client.chat_postMessage(channel: '#taketaketest', text: results1 + "\n" +
+                                                          results2 + "\n" +
+                                                          results3 + "\n" +
+                                                          results4 + "\n" +
+                                                          results5)
