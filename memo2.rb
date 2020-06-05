4a053fd89232831730c8126c94cf7f95fd49300d
Taketoshi Takeuchi <goodgolf722003_2@yahoo.co.jp>
2020-06-06 00:51:12 +0900

    レコード更新

--- memo2.rb
+++ memo2.rb
@@ -6,7 +6,7 @@ require 'CSV'
 
 session = GoogleDrive::Session.from_config("config.json")
 
-rsRow = 443
+rsRow = 3
 
 id = "1dWGVF8kMglGzRpHoAfOM_3l5BAYJX0baJAb2cW_tgnk"
 rs = session.spreadsheet_by_key(id).worksheet_by_title("シート1")
